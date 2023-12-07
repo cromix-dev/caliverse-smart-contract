b2f230727690718cb7052767f12bbc64bdc8ba13
Namhoon Lee <frontalnh@gmail.com>
2023-12-08 08:57:01 +0900

    update

--- migrations/1_migrations.js
+++ migrations/1_migrations.js
@@ -5,13 +5,7 @@ const GeneralERC721V1 = artifacts.require('../contracts/GeneralERC721/GeneralERC
 const GeneralERC721Factory = artifacts.require('../contracts/GeneralERC721/GeneralERC721Factory.sol');
 const LibNFTAdmin = artifacts.require('../contracts/libraries/LibNFTAdmin.sol');
 const LibSale = artifacts.require('../contracts/libraries/LibSale.sol');
-const StakingContractV1 = artifacts.require('../contracts/StakingContract/StakingContractV1.sol');
-const StakingProxyAdmin = artifacts.require('../contracts/StakingContract/StakingProxyAdmin.sol');
-const StakingProxy = artifacts.require('../contracts/StakingContract/StakingProxy.sol');
-
-const TransparentUpgradeableProxy = artifacts.require(
-  '@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol',
-);
+const StakingContract = artifacts.require('../contracts/StakingContract/StakingContract.sol');
 
 const baseUri = 'https://dev-cdn.caliverse.io/contracts/';
 
@@ -24,10 +18,12 @@ module.exports = async (deployer, network, accounts) => {
   await deployer.link(LibNFTAdmin, GeneralERC721V1);
   await deployer.link(LibSale, GeneralERC721V1);
 
-  const libnftAdmin = await LibNFTAdmin.at('0xBdEA81ac0A9658416fa669A0A8e439fAb9A519Cc');
-  const libsale = await LibSale.at('0x35D22D1C9F341105f1813E1E8725f6AD6DE6E815');
-  await deployer.link(libnftAdmin, GeneralERC721V1);
-  await deployer.link(libsale, GeneralERC721V1);
+  // START 이미 배포된 라이브러리 사용하고 싶은 경우
+  // const libnftAdmin = await LibNFTAdmin.at('0xBdEA81ac0A9658416fa669A0A8e439fAb9A519Cc');
+  // const libsale = await LibSale.at('0x35D22D1C9F341105f1813E1E8725f6AD6DE6E815');
+  // await deployer.link(libnftAdmin, GeneralERC721V1);
+  // await deployer.link(libsale, GeneralERC721V1);
+  // END 이미 배포된 라이브러리 사용하고 싶은 경우
 
   await deployer.deploy(GeneralERC721V1);
   setConfig('deployed.' + network + '.GeneralERC721V1', GeneralERC721V1.address);
@@ -35,12 +31,6 @@ module.exports = async (deployer, network, accounts) => {
   setConfig('deployed.' + network + '.GeneralERC721Factory', GeneralERC721Factory.address);
 
   // 스테이킹 컨트렉트 배포
-  await deployer.deploy(StakingProxyAdmin);
-  setConfig('deployed.' + network + '.StakingProxyAdmin', StakingProxyAdmin.address);
-  await deployer.deploy(StakingContractV1);
-  setConfig('deployed.' + network + '.StakingContractV1', StakingContractV1.address);
-  await deployer.deploy(StakingProxy, StakingContractV1.address, StakingProxyAdmin.address);
-  setConfig('deployed.' + network + '.StakingProxy', StakingProxy.address);
-  const stakingProxy = await StakingContractV1.at(StakingProxy.address);
-  await stakingProxy.initialize();
+  await deployer.deploy(StakingContract);
+  setConfig('deployed.' + network + '.StakingContract', StakingContract.address);
 };
